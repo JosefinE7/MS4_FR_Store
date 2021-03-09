45466efd9ad4fdd47a6cde3c361bee2084bb5b62
JosefinE7 <fin.jos@hotmail.com>
2021-03-09 19:45:06 +0000

    Changed carousel nav btns to large

--- home/views.py
+++ home/views.py
@@ -1,6 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
+
 
 def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def all_products(request):
+    """ A view to show all products, including sorting and seach queries """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
