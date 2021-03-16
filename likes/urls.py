844b36f996e7d029acfcc9e685cd0eda4a530475
JosefinE7 <fin.jos@hotmail.com>
2021-03-16 16:55:57 +0000

    Added 'add to likes' functionality

--- likes/urls.py
+++ likes/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_likes, name='view_likes')
+    path('', views.view_likes, name='view_likes'),
+    path('add/<item_id>/', views.add_to_likes, name='add_to_likes'),
+
 ]
--- likes/views.py
+++ likes/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect
+
 
 def view_likes(request):
     """ A view to renders the likes contents page """
 
     return render(request, 'likes/likes.html')
 
+
+def add_to_likes(request, item_id):
+    """ Add a product to the likes category """
+
+    redirect_url = request.POST.get('redirect_url')
+    likes = request.session.get('likes', {})
+
+    request.session['likes'] = likes
+    print(request.session['likes'])
+    return redirect(redirect_url)
