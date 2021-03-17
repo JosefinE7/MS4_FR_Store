87286ef5db7030e6f5b82afa62fdf889806fe21b
JosefinE7 <fin.jos@hotmail.com>
2021-03-17 10:00:22 +0000

    Fixed like function so products are successfully added

--- like/contexts.py
+++ like/contexts.py
@@ -6,17 +6,24 @@ from products.models import Product
 def like_contents(request):
 
     like_items = []
+    total = 0
+    product_count = 0
     like = request.session.get('like', {})
 
-    for item_id in like.items():
+    for item_id, quantity in like.items():
         product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
         like_items.append({
             'item_id': item_id,
+            'quantity': quantity,
             'product': product,
         })
 
     context = {
         'like_items': like_items,
+        'total': total,
+        'product_count': product_count,
     }
 
     return context
--- like/views.py
+++ like/views.py
@@ -10,11 +10,14 @@ def view_like(request):
 def add_to_like(request, item_id):
     """ Add a product to the like category """
 
+    quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     like = request.session.get('like', {})
 
     if item_id in list(like.keys()):
-        like[item_id]
+        like[item_id] += quantity
+    else:
+        like[item_id] = quantity
 
     request.session['like'] = like
     print(request.session['like'])
