8c70818e59fb3f334baeb6a3a8012bd92365d343
JosefinE7 <fin.jos@hotmail.com>
2021-03-20 20:19:49 +0000

    Added Juicer embedded instagram feed to home page

--- home/views.py
+++ home/views.py
@@ -17,16 +17,4 @@ def all_products(request):
         'products': products,
     }
 
-    return render(request, 'products/products.html', context)
-
-
-def product_detail(request, product_id):
-    """ A view to show individual product details """
-
-    product = get_object_or_404(Product, pk=product_id)
-
-    context = {
-        'product': product,
-    }
-
-    return render(request, 'products/product_detail.html', context)
+    return render(request, 'home/index.html', context)
