e332d13e094601c5256e9c9adfd5ffba220228a1
JosefinE7 <fin.jos@hotmail.com>
2021-03-23 12:09:27 +0000

    Deployment to heroku pt2

--- future_retro/settings.py
+++ future_retro/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '44%9y3@l6v!by^12vmrm72r8)yp#21unyb3+-v=hh=sxrrs9s2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['futureretro-store.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -111,12 +111,17 @@ WSGI_APPLICATION = 'future_retro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
