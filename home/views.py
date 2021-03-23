dcd5d88df4e9a74c5fc9d113ad122d5899952a36
JosefinE7 <fin.jos@hotmail.com>
2021-03-23 10:29:00 +0000

    Removed unnecessary and unuseful code from home models and views

--- home/views.py
+++ home/views.py
@@ -1,20 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Product
+from django.shortcuts import render
 
 
 def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
-
-
-def all_products(request):
-    """ A view to show all products, including sorting and seach queries """
-
-    products = Product.objects.all()
-
-    context = {
-        'products': products,
-    }
-
-    return render(request, 'home/index.html', context)
