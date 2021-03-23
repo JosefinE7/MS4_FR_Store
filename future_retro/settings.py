ab55c7f048a5ebf96116441e3430364553607d6a
JosefinE7 <fin.jos@hotmail.com>
2021-03-23 12:22:07 +0000

    Deployment pt3 - removed secret key and set debug

--- future_retro/settings.py
+++ future_retro/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '44%9y3@l6v!by^12vmrm72r8)yp#21unyb3+-v=hh=sxrrs9s2'
+SECRET_KEY = os.environ.get('SECRET_KEY_FR', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['futureretro-store.herokuapp.com', 'localhost']
 
