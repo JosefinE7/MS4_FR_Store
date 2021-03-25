97268aaaca07e54951f2db3b6ed3c014f3e7af66
JosefinE7 <fin.jos@hotmail.com>
2021-03-25 20:55:41 +0000

    Added proper view and css to owl carousel on home page

--- home/views.py
+++ home/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from products.models import Product
 
 
 def index(request):
     """ A view to return the index page """
 
-    return render(request, 'home/index.html')
+    products = Product.objects.filter(category__name='earrings')
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'home/index.html', context)
