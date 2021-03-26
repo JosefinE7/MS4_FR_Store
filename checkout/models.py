1b68cca0809dea080de846100be5fcfd066a09ef
JosefinE7 <fin.jos@hotmail.com>
2021-03-26 15:56:57 +0000

    Added checkout success logic

--- checkout/models.py
+++ checkout/models.py
@@ -37,17 +37,17 @@ class Order(models.Model):
         """
         return uuid.uuid4().hex.upper()
 
-    def update(self):
+    def update_total(self):
         """
         Update grand total each time a line is added,
         accounting for delivery cost
         """
         self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))[
             'lineitem_total__sum'
-        ]
+        ] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = (
-                self.order_total * settings.STANDARD_DELIVERY_PRECENTAGE/100
+                self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE/100
             )
         else:
             self.delivery_cost = 0
