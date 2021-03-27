ae1d575486bf6caa5932b40ee09e73890bb8efce
JosefinE7 <fin.jos@hotmail.com>
2021-03-27 17:57:15 +0000

    Stripe problem fixed, Stripe wh working properly

--- future_retro/settings.py
+++ future_retro/settings.py
@@ -123,8 +123,9 @@ WSGI_APPLICATION = 'future_retro.wsgi.application'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL')),
     }
+
 else:
     DATABASES = {
         'default': {
