49cb3d72606aca053c151ab0386ff87d377de112
JosefinE7 <fin.jos@hotmail.com>
2021-03-29 13:56:52 +0000

    Added login_required to Product Admin views

--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
-
+from django.contrib.auth.decorators import login_required
 from .models import UserProfile
 from .forms import UserProfileForm
 
 from checkout.models import Order
 
 
+@login_required
 def profile(request):
     """ Display the user profile """
     profile = get_object_or_404(UserProfile, user=request.user)
@@ -35,6 +36,7 @@ def profile(request):
     return render(request, template, context)
 
 
+@login_required
 def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
 
